0da808f7dfc9eeb8eb48584d09d32821872e861e
eavictor <eavictor@me.com>
2015-09-27 19:24:23 +0800

    Update "UpdateMethods" with hibernate
    new problem : slow insert speed
    
    only 1/8 compare with JDBC

--- GeoIP/src/com/eavictor/model/UpdateMethods.java
+++ GeoIP/src/com/eavictor/model/UpdateMethods.java
@@ -24,6 +24,10 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 import org.apache.commons.io.FileUtils;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import hibernate.util.HibernateUtil;
 
 public class UpdateMethods {
 	private String path;
@@ -31,6 +35,7 @@ public class UpdateMethods {
 	private String csvFile = "countryip.csv";
 	private static final String TRUNCATE = "TRUNCATE TABLE GEOIP";
 	private static final String INSERT = "INSERT INTO GEOIP (ip_start,ip_end,country) VALUES(?,?,?)";
+	private static final String hibernate_TRUNCATE = "truncate table GEOIP";
 	private DataSource ds = null;
 
 	public UpdateMethods() {
@@ -78,11 +83,8 @@ public class UpdateMethods {
 	public boolean unZip() {
 		System.out.println("start unzip");
 		byte[] buffer = new byte[8192];
-		GZIPInputStream inputStream = null;
-		FileOutputStream outputStream = null;
-		try {
-			inputStream = new GZIPInputStream(new FileInputStream(path + gzipFile));
-			outputStream = new FileOutputStream(path + csvFile);
+		try (GZIPInputStream inputStream = new GZIPInputStream(new FileInputStream(path + gzipFile));
+				FileOutputStream outputStream = new FileOutputStream(path + csvFile);) {
 			int length;
 			while ((length = inputStream.read(buffer)) > 0) {
 				outputStream.write(buffer, 0, length);
@@ -92,110 +94,146 @@ public class UpdateMethods {
 		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
-		} finally {
-			if (inputStream != null) {
-				try {
-					inputStream.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			if (outputStream != null) {
-				try {
-					outputStream.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
 		}
 	}
 
 	public boolean doUpdate() {
-		BufferedReader br = null;
-		String line = null;
-		Connection connection = null;
-		PreparedStatement pstmt = null;
-		try {
-			System.out.println("start update");
-			br = new BufferedReader(new FileReader(path + csvFile));
-			connection = ds.getConnection();
-			List<IPBean> ipBlock = new ArrayList<>();
-			connection.setAutoCommit(false);
-			pstmt = connection.prepareStatement(TRUNCATE);
-			pstmt.executeUpdate();
-			connection.commit();
-			pstmt = connection.prepareStatement(INSERT);
-			line = br.readLine();
-			while (line != null) {
-				String[] stringArray = line.replace("\"", "").split(",");
-				line = br.readLine();
-				IPBean ipBlockBean = new IPBean();
-				ipBlockBean.setIpstart(stringArray[0]);
-				ipBlockBean.setIpend(stringArray[1]);
-				ipBlockBean.setCountry(stringArray[2]);
-				ipBlock.add(ipBlockBean);
+		List<IPBean> ipbeans = new ArrayList<IPBean>();
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		int count = 0;
+		try (
+				BufferedReader br = new BufferedReader(new FileReader(path + csvFile));
+				) {
+			
+			// wipe old data
+			session.beginTransaction();
+			session.createSQLQuery(hibernate_TRUNCATE).executeUpdate(); //do not commit !!
+			
+			// read IP block from file, and store it into a list
+			String oneLine = br.readLine();
+			while (oneLine != null) {
+				String[] oneLineMatrix = oneLine.split(",");
+				IPBean ipbean = new IPBean();
+				ipbean.setIpstart(oneLineMatrix[0].replace("\"",""));
+				ipbean.setIpend(oneLineMatrix[1].replace("\"",""));
+				ipbean.setCountry(oneLineMatrix[2].replace("\"",""));
+				ipbeans.add(ipbean);
+				oneLine = br.readLine();
 			}
-			System.out.println("read from csv file complete");
-			System.out.println("insert new data into database");
-			int count = 0;
-			for (IPBean ipBlockBean : ipBlock) {
-				pstmt.setString(1, ipBlockBean.getIpstart());
-				pstmt.setString(2, ipBlockBean.getIpend());
-				pstmt.setString(3, ipBlockBean.getCountry());
-				pstmt.addBatch();
-				count++;
-				if (count % 1000 == 0) {
-					System.out.println("execute batch update "+Math.floor(count/1000));
-					pstmt.executeBatch();
-					connection.commit();
+			
+			// insert into database
+			try {
+				session.beginTransaction();
+				for (IPBean ipbean : ipbeans) {
+					session.saveOrUpdate(ipbean);
+					count++;
+					if (count%1000==0){
+						System.out.println(count);
+					}
 				}
+				System.out.println(count);
+				session.getTransaction().commit();
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-			System.out.println("execute batch update (final)");
-			pstmt.executeBatch();
-			connection.commit();
-			System.out.println("commit OK");
-			connection.setAutoCommit(true);
-			System.out.println("set auto commit true");
+			
+			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-			if (connection != null) {
-				try {
-					connection.rollback();
-				} catch (SQLException e1) {
-					e1.printStackTrace();
-				}
-			}
-			return false;
-		} catch (SQLException e) {
+		} catch (HibernateException e) {
 			e.printStackTrace();
-			if (connection != null) {
-				try {
-					connection.rollback();
-				} catch (SQLException e1) {
-					e1.printStackTrace();
-				}
-			}
-			return false;
 		} catch (IOException e) {
 			e.printStackTrace();
-			if (connection != null) {
-				try {
-					connection.rollback();
-				} catch (SQLException e1) {
-					e1.printStackTrace();
-				}
-			}
-			return false;
-		} finally {
-			if (br != null) {
-				try {
-					br.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
 		}
 		return true;
 	}
+	
+//	public boolean doUpdate() {
+//		BufferedReader br = null;
+//		String line = null;
+//		Connection connection = null;
+//		PreparedStatement pstmt = null;
+//		try {
+//			System.out.println("start update");
+//			br = new BufferedReader(new FileReader(path + csvFile));
+//			connection = ds.getConnection();
+//			List<IPBean> ipBlock = new ArrayList<>();
+//			connection.setAutoCommit(false);
+//			pstmt = connection.prepareStatement(TRUNCATE);
+//			pstmt.executeUpdate();
+//			connection.commit();
+//			pstmt = connection.prepareStatement(INSERT);
+//			line = br.readLine();
+//			while (line != null) {
+//				String[] stringArray = line.replace("\"", "").split(",");
+//				line = br.readLine();
+//				IPBean ipBlockBean = new IPBean();
+//				ipBlockBean.setIpstart(stringArray[0]);
+//				ipBlockBean.setIpend(stringArray[1]);
+//				ipBlockBean.setCountry(stringArray[2]);
+//				ipBlock.add(ipBlockBean);
+//			}
+//			System.out.println("read from csv file complete");
+//			System.out.println("insert new data into database");
+//			int count = 0;
+//			for (IPBean ipBlockBean : ipBlock) {
+//				pstmt.setString(1, ipBlockBean.getIpstart());
+//				pstmt.setString(2, ipBlockBean.getIpend());
+//				pstmt.setString(3, ipBlockBean.getCountry());
+//				pstmt.addBatch();
+//				count++;
+//				if (count % 1000 == 0) {
+//					System.out.println("execute batch update "+Math.floor(count/1000));
+//					pstmt.executeBatch();
+//					connection.commit();
+//				}
+//			}
+//			System.out.println("execute batch update (final)");
+//			pstmt.executeBatch();
+//			connection.commit();
+//			System.out.println("commit OK");
+//			connection.setAutoCommit(true);
+//			System.out.println("set auto commit true");
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//			if (connection != null) {
+//				try {
+//					connection.rollback();
+//				} catch (SQLException e1) {
+//					e1.printStackTrace();
+//				}
+//			}
+//			return false;
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//			if (connection != null) {
+//				try {
+//					connection.rollback();
+//				} catch (SQLException e1) {
+//					e1.printStackTrace();
+//				}
+//			}
+//			return false;
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			if (connection != null) {
+//				try {
+//					connection.rollback();
+//				} catch (SQLException e1) {
+//					e1.printStackTrace();
+//				}
+//			}
+//			return false;
+//		} finally {
+//			if (br != null) {
+//				try {
+//					br.close();
+//				} catch (IOException e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		}
+//		return true;
+//	}
 
 }
